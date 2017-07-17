6d8f8d95c6b248284c46a30c45e7b9192d91f506
Beau <beaugalbraith@gmail.com>
2017-07-17 10:15:19 -0500

    UI setup

---
+++ FlickFindr/Constants.swift
@@ -0,0 +1,10 @@
+//
+//  Constants.swift
+//  FlickFindr
+//
+//  Created by gem on 7/17/17.
+//  Copyright © 2017 beau. All rights reserved.
+//
+
+import Foundation
+let APIKEY = "7187e98d9f0b9cac0890d14b901e693a"
--- FlickFindr/ViewController.swift
+++ FlickFindr/ViewController.swift
@@ -9,17 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var mainLabel: UILabel!
+    @IBOutlet weak var flickrImage: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBAction func latLonSearched(_ sender: UIButton) {
+    }
+    @IBOutlet weak var searchField: UITextField!
+    @IBAction func termSearched(_ sender: UIButton) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-
 
 }
 
