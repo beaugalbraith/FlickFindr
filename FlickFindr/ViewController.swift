70f68abf08290206e87256bf8247af714b0727ea
Beau <beaugalbraith@gmail.com>
2017-07-17 09:18:47 -0500

    Initial Commit

---
+++ FlickFindr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FlickFindr
+//
+//  Created by gem on 7/17/17.
+//  Copyright © 2017 beau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
